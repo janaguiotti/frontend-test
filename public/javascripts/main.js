362974ca82c412effe68756b225262685c95aff7
janaguiotti <jana.guiotti@gmail.com>
2018-07-10 14:15:44 -0300

    add table with count votes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,5 @@
-var url='../fazenda.json'; // relative url : 'data.json'; protocole-relative absolute url: '//website.org/path/to/data.json';
+var url='../fazenda.json'; 
 
-//4a.function creation
 var slingshot = function (url, tplId, anchor) {
 $.getJSON(url, function(data) {
 	var template = $(tplId).html();
@@ -8,5 +7,11 @@ $.getJSON(url, function(data) {
 	$(anchor).append(stone);
 	});
 }
-//4b.function firing
-slingshot('../fazenda.json', '#tpl', '#anchor'); // since url = 'data.json' , we can use both notations.
+slingshot('../fazenda.json', '#tpl', '#anchor'); 
+
+//count votes
+$(document).ready(function() {
+	$('.js-click-votes').click(function(){
+		$(this).toggleClass('table__row--modify');
+	});
+});
