f48b2f9620c8f5dc95d37dcc60c551948eb82419
janaguiotti <jana.guiotti@gmail.com>
2018-07-09 23:12:18 -0300

    add artists with handlebar and json

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,12 @@
-// your code
+var url='../fazenda.json'; // relative url : 'data.json'; protocole-relative absolute url: '//website.org/path/to/data.json';
+
+//4a.function creation
+var slingshot = function (url, tplId, anchor) {
+$.getJSON(url, function(data) {
+	var template = $(tplId).html();
+	var stone = Handlebars.compile(template)(data);
+	$(anchor).append(stone);
+	});
+}
+//4b.function firing
+slingshot('../fazenda.json', '#tpl', '#anchor'); // since url = 'data.json' , we can use both notations.
